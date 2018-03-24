4c261740dc3f272dcbfe9a8edc83024a891648eb
ekinsert <ekinsert93@gmail.com>
2018-03-24 10:29:23 +0300

    Add files via upload
    There are some changes could you please check the "finding word from last letter"

--- Dictionary.java
+++ Dictionary.java
@@ -32,4 +32,40 @@ public class Dictionary {
     
     }
     
+    public static String giveWordbyLetter(String s) {
+    
+    String[] dict = new String[75];
+ 
+    dict[0] = "elephant"; dict[1] = "aeroplane"; dict[2] = "chicago"; dict[3] = "giraffe"; dict[4] = "microscope";
+    dict[5] = "vegetable"; dict[6] = "biological"; dict[7] = "purple"; dict[8] = "desire"; dict[9] = "space";
+    dict[10] = "article"; dict[11] = "accomplishment"; dict[12] = "beach"; dict[13] = "brave"; dict[14] = "curriculum"; 
+    dict[15] = "card"; dict[16] = "development"; dict[17] = "delicious"; dict[18] = "geography"; dict[19] = "gigabyte"; 
+    dict[20] = "hesitation"; dict[21] = "history"; dict[22] = "insect"; dict[23] = "incredible"; dict[24] = "importing";
+    dict[25] = "jansen"; dict[26] = "jungle"; dict[27] = "justify"; dict[28] = "kid"; dict[29] = "kenya";
+    dict[30] = "kangroo"; dict[31] = "lung"; dict[32] = "linguistic"; dict[33] = "local"; dict[34] = "massive"; 
+    dict[35] = "mandatory"; dict[36] = "nephew"; dict[37] = "newspaper"; dict[38] = "north"; dict[39] = "optimistic"; 
+    dict[40] = "optimal"; dict[41] = "oligarchy"; dict[42] = "pearl"; dict[43] = "philosophy"; dict[44] = "realistic";
+    dict[45] = "report"; dict[46] = "recognize"; dict[47] = "selfy"; dict[48] = "scientific"; dict[49] = "telegraph";
+    dict[50] = "trasportation"; dict[51] = "traffic"; dict[52] = "universe"; dict[53] = "ultimate"; dict[54] = "ultimatum";
+    dict[55] = "virtual"; dict[56] = "vertical"; dict[57] = "yankee"; dict[58] = "yatching"; dict[59] = "yesterday";
+    dict[60] = "zoo"; dict[61] = "zeppelin"; dict[62] = "zipper"; dict[63] = "washington"; dict[64] = "welding";
+    dict[65] = "wall"; dict[66] = "xenon"; dict[67] = "xray"; dict[68] = "xylography"; dict[69] = "enemy";
+    dict[70] = "eclipse"; dict[71] = "yield"; dict[72] = "contrast"; dict[73] = "global"; dict[74] = "neon";
+    
+    double rd = Math.random()*75;
+    
+    int ri = (int)rd;
+    
+    String rw = dict[ri];
+    
+    if (s.equals(rw.substring(0,1))) {
+        
+    return rw;
+    
+    }
+    
+    return giveWordbyLetter(s);
+    
+    }
+    
 }
--- FindingWordFromLastSentence.java
+++ FindingWordFromLastSentence.java
@@ -3,31 +3,83 @@ package programming_project;
 
 import java.util.Scanner;
 
+import static programming_project.Dictionary.giveWordbyLetter;
+
 public class FindingWordFromLastSentence {
     
     public static void main(String[] args) {
         
-        boolean d = false;
+        boolean d = false; // will be used to check if the first letter of player is equal to last letter of cpu.
         
-        int oe = 0; // odd or even.
+        int oe = 0; // odd or even number. (to remain frequency btw user and cpu)
+        
+        String[] usedwords = new String[150]; // to keep used words.
+        
+        for(int k = 0; k < 150; k++) {
+            
+        usedwords[k] = "xxxx"; // default values for array members.
+            
+        }
+        
+        int iou = 0; // index of usedwords.
+        
+        int louw = 0; // length od usedwors array(150)
         
         Scanner scan = new Scanner(System.in);
         
         System.out.print("Please enter the first word: ");
         
-        String user = scan.nextLine();
+        String user = scan.nextLine(); // first word of the game.
+        
+        usedwords[iou] = user; // to add the word to used words.
+        
+        iou++;
         
         int i = user.length();
         
-        String llu = String.valueOf(user.charAt(i-1));
+        String llu = String.valueOf(user.charAt(i-1)); // last letter of user's entry.
         
         do {
             
         if(oe%2 == 0) {
-                 
+      
         System.out.print("CPU is entering the next word: ");
         
-        String cpu = scan.nextLine();
+        String cpu = giveWordbyLetter(llu); // creates a word starts with last letter of user entry.
+        
+        louw = usedwords.length; // length of udeswords array.
+      
+        for(int v = 0; v < louw; v++) { // checks if the entry used or not.
+            
+        if (cpu.equals(usedwords[v])) { // SETH COULD YOU PLEASE CHECK HERE, IT DOES NOT COMPERE TWO WORDS.
+         
+        cpu = giveWordbyLetter(llu); // gives another random word starts with last letter of user for cpu.
+        
+        if (v == louw-1) {
+            
+        System.out.println("Player Wins!");
+            
+        System.exit(0);
+            
+        }
+       
+        }
+        
+        else {
+            
+        System.out.println("tried C");
+            
+        v = louw;
+        
+        }
+
+        }
+        
+        System.out.println(cpu);
+        
+        usedwords[iou] = cpu;
+        
+        iou++;
         
         int a = cpu.length();
         
@@ -47,23 +99,43 @@ public class FindingWordFromLastSentence {
                  
         System.out.print("Player is entering the next word: ");
         
-        String cpu = scan.nextLine();
+        String player = scan.nextLine();
         
-        int a = cpu.length();
+        louw = usedwords.length; // length of udeswords array.
+      
+        for(int v = 0; v < louw; v++) { // checks if the entry used or not.
+
+        if (player.equals(usedwords[v])) {
+           
+        System.out.println("CPU Wins!");
         
-        String flc = String.valueOf(cpu.charAt(0));
+        System.exit(0);
+            
+        }
         
-        String llc = String.valueOf(cpu.charAt(a-1));
+        }
         
-        d = flc.equals(llu);
+        usedwords[iou] = player;
         
-        llu = llc;
+        iou++;
+        
+        int a = player.length();
+        
+        String flp = String.valueOf(player.charAt(0)); // first letter of player.
+        
+        String llp = String.valueOf(player.charAt(a-1)); // last letter of player.
+        
+        d = flp.equals(llu);
+        
+        llu = llp; 
        
         oe++;
         
         }
        
         } while(d);
+        
+        System.out.println("CPU Wins!");
         
         }
     
