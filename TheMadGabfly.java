b9ca915623891f17e0ba65e89995fce845080a0d
5374m164a1l <36229182+5374m164a1l@users.noreply.github.com>
2018-04-20 12:54:20 +0300

    Update TheMadGabfly.java

--- TheMadGabfly.java
+++ TheMadGabfly.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -35,6 +36,7 @@ public class TheMadGabfly extends Application {
             public void handle(ActionEvent event) {
                 Anagrams pane1=new Anagrams();
                 try {
+                    pane1.title="Anagrams";
                     pane1.init();
                     pane1.start(primaryStage);
                 } catch (Exception ex) {
@@ -51,6 +53,7 @@ public class TheMadGabfly extends Application {
             public void handle(ActionEvent event) {
                 Connections pane2=new Connections();
                 try {
+                    pane2.title="Connections";
                     pane2.init();
                     pane2.start(primaryStage);
                 } catch (Exception ex) {
@@ -67,6 +70,8 @@ public class TheMadGabfly extends Application {
             public void handle(ActionEvent event) {
                 Space_o_plex pane3=new Space_o_plex();
                 try {
+                    pane3.spaceoplex();
+                    pane3.title="Space-o-plex";
                     pane3.init();
                     pane3.start(primaryStage);
                 } catch (Exception ex) {
@@ -83,6 +88,7 @@ public class TheMadGabfly extends Application {
             public void handle(ActionEvent event) {
                 Settings pane4=new Settings();
                 try {
+                    primaryStage.setTitle("Settings");
                     pane4.init();
                     pane4.start(primaryStage);
                 } catch (Exception ex) {
@@ -91,21 +97,33 @@ public class TheMadGabfly extends Application {
                 
             }
         });
+        System.out.println(btn1.getLabelPadding());
+        System.out.println(btn1.getLayoutBounds());
+        System.out.println(btn1.getLayoutX());
+        System.out.println(btn1.getHeight());
+        System.out.println(btn1.getOnAction());
+        System.out.println();
+        
+        //btn1.
+        
         
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setMinSize(300, 300);
+        grid.setMinSize(400, 200);
+        grid.setAlignment(Pos.CENTER);
         grid.setVgap(5);
         grid.setHgap(5);
- 
+        
         grid.add(btn1,2,0);
+        System.out.println(grid.getAlignment());
+                
         grid.add(btn2,2,2);
         grid.add(btn3,2,4);
         grid.add(btn4,2,6);
         
         grid.setStyle("-fx-background-color: #E6E6E6;");
 
-        Scene scene = new Scene(grid, 300, 300);
+        Scene scene = new Scene(grid, 400, 200);
         
         primaryStage.setTitle("TheMadGabFly");
         primaryStage.setScene(scene);
