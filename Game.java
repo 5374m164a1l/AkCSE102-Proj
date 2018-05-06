90e78bb577eea1c616415a0340f2b862a200ea1e
5374m164a1l <36229182+5374m164a1l@users.noreply.github.com>
2018-05-06 10:10:59 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.xml.bind.Marshaller.Listener;
@@ -33,8 +34,7 @@ public abstract class Game extends Application{
     public Button btn3 = new Button();
     public int score = 0;
     public Text points = new Text("Score: " + score);
-    
-    
+        
     public ActionListener bl1 = new ActionListener() {
         @Override
         public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -42,6 +42,7 @@ public abstract class Game extends Application{
             throw new UnsupportedOperationException("Not supported yet."); 
         }
     };
+    
     public static Listener bl2 = new Listener() {};
         
     @Override
@@ -61,6 +62,7 @@ public abstract class Game extends Application{
                     nextStep(pword);
                 }}
         });
+        
         btn2.setText("Start");
         btn2.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -71,21 +73,25 @@ public abstract class Game extends Application{
                 firstStep();
             }
         });
+        
         btn3.setText("Main Menu");//exit settings and return to main menu
         btn3.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                
                 TheMadGabfly again = new TheMadGabfly();
                 try {
                     again.init();
                     again.start(primaryStage); 
-                } catch (Exception ex) {
+                    for (int i = 0; i < 149; i++) {
+                        Connections.usedwords[i]=null;
+                    }
+                }catch (Exception ex) {
                     Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
+        
         in.setPrefColumnCount(7);
         in.setOnAction(new EventHandler<ActionEvent>() {
             
@@ -109,13 +115,17 @@ public abstract class Game extends Application{
         grid.setVgap(5);
         grid.setHgap(5);
         grid.add(btn3,0,0);
+        message.setFont(new Font(20));
+        message.setWrappingWidth(450.0);
         grid.add(message,1,1);
+        line1.setFont(new Font(20));
         grid.add(line1,1,2);
+        line2.setFont(new Font(20));
         grid.add(line2,1,3);
         grid.add(in,1,5);
         grid.add(btn1,1,7);
         grid.add(btn2,1,9);
-        grid.add(points,1,25);
+        grid.add(points,1,14);
 
         grid.setStyle("-fx-background-color: #E6E6E6;");
         
