8594c8ca41d1d94dcbb06e89952d8623b4b61d48
5374m164a1l <36229182+5374m164a1l@users.noreply.github.com>
2018-05-16 18:06:38 +0300

    Update FindingWordFromLastSentence.java

--- FindingWordFromLastSentence.java
+++ FindingWordFromLastSentence.java
@@ -2,7 +2,6 @@ package themadgabfly;
 
 import java.util.HashSet;
 import static themadgabfly.Dictionary.*;
-import static themadgabfly.Settings.loadSaves;
 
 /**
  * @author Seth Michail & Ekin Sert, CSE MSc program
@@ -16,7 +15,7 @@ public class Connections extends Game {//Application {
     
     @Override
     public void nextStep(StringBuilder s){
-        //what to when submit button
+        //what to when submit button pressed
         if(preezent(pword.toString())){
             if(!used(pword.toString())){
                 chkwrd(pword.toString());
@@ -37,15 +36,15 @@ public class Connections extends Game {//Application {
     @Override
     public void firstStep(){
         populate();
-                                //what to when start button
+        //what to when start button pressed
         message.setText("Type a word with first letter same as last letter of word shown below");
         if(cword.length()>0){
             cword.delete(0, cword.length());
-            cword.append(giveWord());
+            cword.append(giveWord());//first word
         }else{
-            cword.append(giveWord());
+            cword.append(giveWord());//also first word
         }       
-        if(!used(cword.toString())){
+        if(!used(cword.toString())){//if not used, add it to used
             usedWords.add(cword.toString());
             line1.setText(cword.toString());       //the word chosen by the computer
             llu = String.valueOf(cword.charAt(cword.length()-1));
@@ -58,13 +57,20 @@ public class Connections extends Game {//Application {
     
     public void computr(){              //this method gets a word for the computer based on the last letter of the users word
         int j = 0;
-        if(cword.length()==0){message.setText("Computer word not given!");return;} //sets message and exits method, because something went whack
-        else{
+        if(cword.length()==0){
+            //sets message and exits method, because something went whack
+            message.setText("Computer word not given!");
+            return;
+        }  
+        else{                                       
             while(used(cword.toString())){
                 cword.delete(0, cword.length());
                 cword.append(giveWordbyLetter(llu));
                 j++;
-                if(j>256){System.out.println("error line 66");break;}           //to prevent infinite loop if all valid words have been used
+                if(j>100){//to prevent infinite loop if all valid words have been used
+                    message.setText("You Win!");
+                    break;
+                }
             }
         }
         if(cword.length()>0){
